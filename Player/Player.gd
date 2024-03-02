6583c07de9699b6708f40f091684c5de85576c86
james <jamesianburns@gmail.com>
2024-03-02 10:10:49 -0500

    feat: added fish models

--- Player/Player.gd
+++ Player/Player.gd
@@ -64,7 +64,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction = lerp(direction ,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+	var direction = lerp(direction ,(transform.basis * Vector3(input_dir.x, 0, -input_dir.y)).normalized(),delta*lerp_speed)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
