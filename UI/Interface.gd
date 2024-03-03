a79f4896ee4807031fdeaf9e02208fa1c8f32655
Apallo2 <taylorkuehm@gmail.com>
2024-03-02 19:54:45 -0800

    Player UI

--- UI/Interface.gd
+++ UI/Interface.gd
@@ -2,7 +2,13 @@ extends Control
 @onready var wiggle_count = $MarginContainer/HBoxContainer/VBoxContainer/WiggleCount
 @onready var health_count = $MarginContainer/HBoxContainer/VBoxContainer/HealthCount
 @onready var game_over = $GameOver
+@onready var heart_container = $MarginContainer/HBoxContainer/VBoxContainer/HeartContainer
 
+
+@export var health_full:AtlasTexture
+@export var health_empty:AtlasTexture
+var max_health:int = 5
+var current_health:int
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,3 +23,12 @@ func _on_player_wiggled(d:Dictionary):
 
 func _on_player_health_changed(d:Dictionary):
 	health_count.text = "Health - %s" % d.player_health
+	current_health = d.player_health
+	for i in max_health:
+		var heart = TextureRect.new()
+		if i < current_health:
+			heart.texture = health_full
+		else:
+			heart.texture = health_empty
+		
+		heart_container.add_child(heart)
