bc7ae36a35e2eb41cd7d2b7cb42e035b0cc75916
james <jamesianburns@gmail.com>
2024-03-02 22:54:31 -0500

    fix: start game

--- LevelStartDisplay.gd
+++ LevelStartDisplay.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func show_for_level_start(level: int):
-	$CenterContainer/VBoxContainer/LevelStartLabel.text = "LEVEL %d START" % level
+	$CenterContainer/LevelStartLabel.text = "LEVEL %d START" % level
 	visible = true
 	$Timer.start()
 	
